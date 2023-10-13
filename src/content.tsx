157769fada5020bab3b14c3e33fa55542ab4cc5e
acgriswold <agriswoldcamp@gmail.com>
2023-10-13 13:58:11 -0400

    ✨(src): migrate to tsx

---
+++ src/content.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function Content() {
+    return (
+        <>
+            <h1>🚀 Learning at speed</h1>
+            <div className="text-subtle">Let's get back to the basics</div>
+        </>
+    )
+}
--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,15 @@
+import { render } from 'react-dom';
+
 import './style.css';
+import { Content } from './content';
 
 function init() {
   const app = document.querySelector('#app')
 
   if (!app)
     return
-  
-  const header = document.createElement('h1')
 
-  const headerText = document.createTextNode(`🚀 Learning at speed`)
-
-  header.appendChild(headerText)
-
-  const description = document.createElement('div')
-  description.className = 'text-subtle'
-
-  const desciptionText = document.createTextNode('Let\'s get back to the basics')
-
-  description.appendChild(desciptionText)
-
-  app.appendChild(header)
-  app.appendChild(description)
+  render(Content(), app);
 }
 
 init()
