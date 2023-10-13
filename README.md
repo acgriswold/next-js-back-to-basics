3d7a2b1dfbf3049603b6150220e9f0f24ecede61
acgriswold <34039420+acgriswold@users.noreply.github.com>
2023-10-12 21:07:41 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# next-js-back-to-basics
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/acgriswold/next-js-back-to-basics)
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import './style.css';
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Hello Vite!</h1>
+    
+    <div class="text-subtle">
+      Let's get back to the basics
+    </div>
+  </div>
+`;
